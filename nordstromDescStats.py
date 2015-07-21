5fbb97b9fbd4c195f8a348c9deedea4f7e9b5e8d
BEugeneSmith7 <brianesmith11@gmail.com>
2015-07-21 18:19:12 -0500

    ordination

--- nordstromDescStats.py
+++ nordstromDescStats.py
@@ -2,13 +2,13 @@ from collections import OrderedDict
 import re
 
 class Props:
-    
+
     def __init__(self):
         self.items = self.__pieces()
         self.fits = self.__fits()
         self.patterns = self.__patterns()
         self.textures = self.__textures()
-        
+
     def __pieces(self):
     #list of all the pieces we may want to look for in either collection
         return(
@@ -26,7 +26,7 @@ class Props:
                 #eliminated Dress for now
             ]
         )
-    
+
     def __fits(self):
     #list of all the fits we may want to look for in either collection
         return(
@@ -37,7 +37,7 @@ class Props:
                 'Boxy','Oversize','Bootcut'
             ]
         )
-        
+
     def __patterns(self):
     #list of all the patterns we may want to look for in either collection
         return(
@@ -48,7 +48,7 @@ class Props:
                 'Chevron'
             ]
         )
-    
+
     def __textures(self):
     #list of all the textures we may want to look for in either collection
         return(
@@ -61,9 +61,9 @@ class Props:
                 'Textured','Ruffled','Crinkled','Fur','Slub'
             ]
         )
-    
+
 class dfExtract(Props):
-    
+
     def __init__(self,df):
         Props.__init__(self)
         self.df = df
@@ -72,39 +72,39 @@ class dfExtract(Props):
         self.FitFreqCnt = self.__fitFreqCount()
         self.PatternFreqCnt = self.__patternFreqCount()
         self.TextureFreqCnt = self.__textureFreqCount()
-        
+
     def __itemFreqTable(self):
     #creates a table of frequency for all of the item names
         itemNames = self.df['ItemName']
         terms = (' '.join(itemNames.tolist())).split(' ')
-        
+
         freqTable = {}
         for term in terms:
             freqTable[term] = freqTable.get(term,0)+1
-     
+
         return(freqTable)
-    
+
     def __itemFreqCount(self):
     #returns the counts of ONLY the selected items
         itemCounts = []
         for item in self.items:
             itemCounts.append(self.itemFreqTable.get(item))
         return(itemCounts)
-    
+
     def __fitFreqCount(self):
     #returns the counts of ONLY the selected fits
         fitCounts = []
         for fit in self.fits:
             fitCounts.append(self.itemFreqTable.get(fit))
         return(fitCounts)
-    
+
     def __textureFreqCount(self):
     #returns the counts of ONLY the selected textures
         textureCounts = []
         for texture in self.textures:
             textureCounts.append(self.itemFreqTable.get(texture))
         return(textureCounts)
-    
+
     def __patternFreqCount(self):
     #returns the counts of ONLY the selected patterns
         patternCounts = []
@@ -114,12 +114,12 @@ class dfExtract(Props):
 
 
 class ratioAnalysis:
-    
+
     def __init__(self,mStats,wStats):
         self.menStats = mStats
         self.womenStats = wStats
         self.ratioData = self.ratioPrep()
-    
+
     def ratioPrep(self):
         menSummNorm = [float(i)/sum(self.menStats.summary.values()) for i in self.menStats.summary.values()]
         menuSummNorm = [float(i)/sum(self.menStats.uniqueSummary.values()) for i in self.menStats.uniqueSummary.values()]
@@ -133,24 +133,24 @@ class ratioAnalysis:
             'women Fit', 'women Texture', 'women Patterns', 'women Items'
             ]
         ratio=[]
-    
+
         for i in range(8):
             ratio.append(uSumm[i]/Summ[i])
-    
+
         return({'x':Summ,'y':uSumm,'desc':desc,'ratio':ratio})
-    
+
 class descriptiveStats(Props):
-    
+
     def __init__(self,df):
         self.data = dfExtract(df)
         self.tFits = self.__totalFits()
         self.tTextures = self.__totalTextures()
         self.tItems = self.__totalItems()
         self.tPatterns = self.__totalPatterns()
-    
+
         self.summary = self.summary()
         self.uniqueSummary = self.uniqueSummary()
-        
+
     def __superSum(self,array):
     #Temporary solution; this skips things that will muck up our totals
         total = 0
@@ -159,26 +159,26 @@ class descriptiveStats(Props):
                 total += i
             except:
                 next
-        return(total) 
-    
-    
+        return(total)
+
+
     def __totalFits(self):
     #total number of fits counted
         return(self.__superSum(self.data.FitFreqCnt))
-        
+
     def __totalTextures(self):
     #total number of textures counted
         return(self.__superSum(self.data.TextureFreqCnt))
-    
-            
+
+
     def __totalItems(self):
     #total number of items counted
         return(self.__superSum(self.data.ItemFreqCnt))
-    
+
     def __totalPatterns(self):
     #total number of patterns counted
         return(self.__superSum(self.data.PatternFreqCnt))
-    
+
     def summary(self):
     #returns summary of the total counts for the dataset
         return(
@@ -189,7 +189,7 @@ class descriptiveStats(Props):
                 'Patterns':self.tPatterns
             }
         )
-    
+
     def uniqueSummary(self):
     #returns summary of the total unique counts for the dataset
         return(
@@ -200,9 +200,9 @@ class descriptiveStats(Props):
                 'Patterns':len(filter(None,self.data.PatternFreqCnt))
             }
         )
-        
+
 class plotPrep(Props):
-    
+
     def __init__(self,df):
         Props.__init__(self)
         self.data = dfExtract(df)
@@ -210,7 +210,7 @@ class plotPrep(Props):
         self.itemZip = self.__itemZip()
         self.textureZip = self.__textureZip()
         self.patternZip = self.__patternZip()
-    
+
     def __fitZip(self):
     #combines select fit data
         newDict = {}
@@ -224,7 +224,7 @@ class plotPrep(Props):
 
         sortedDict = OrderedDict(sorted(newDict.items(),key=lambda t:t[0]))
         return(sortedDict)
-    
+
     def __itemZip(self):
     #combines select item data
         newDict = {}
@@ -238,7 +238,7 @@ class plotPrep(Props):
 
         sortedDict = OrderedDict(sorted(newDict.items(),key=lambda t:t[0]))
         return(sortedDict)
-    
+
     def __textureZip(self):
     #combines select texture data
         newDict = {}
@@ -252,7 +252,7 @@ class plotPrep(Props):
 
         sortedDict = OrderedDict(sorted(newDict.items(),key=lambda t:t[0]))
         return(sortedDict)
-    
+
     def __patternZip(self):
     #combines select pattern data
         newDict = {}
@@ -273,26 +273,26 @@ class colorDataPrep:
         self.freqTable = self.__colorFreqTable()
         self.totalColors = sum(self.freqTable.values())
         self.ratioTable = self.__percentageTable()
-        
+
     def __colorFreqTable(self):
         colors = self.df['binnedColor'].tolist()
-        
+
         freqTable = {}
         for color in colors:
             freqTable[color] = freqTable.get(color,0)+1
-     
+
         return(freqTable)
-    
+
     def __percentageTable(self):
         ratio = self.freqTable
-        
+
         for color in ratio.keys():
             perc = 100 * (ratio[color]/float(self.totalColors))
             ratio[color] = perc
         return(ratio)
 
 class itemReduce(Props):
-    
+
     def __init__(self,df):
         Props.__init__(self)
         self.df = df
@@ -300,8 +300,9 @@ class itemReduce(Props):
         self.df['pattern'] = self.__extractedPatterns()
         self.df['texture'] = self.__extractedTextures()
         self.df['fit'] = self.__extractedFits()
-    
+
     def __translatePiece(self,item):
+    # Bins pieces
         for piece in self.items:
             testPiece = re.findall(piece,item)
             if testPiece:
@@ -310,17 +311,18 @@ class itemReduce(Props):
                 return ['unsorted']
             else:
                 next
-            
+
     def __extractedPieces(self):
         array = []
         for item in self.df['ItemName']:
             array.append(self.__translatePiece(item))
         combinedArray = sum(array,[])
         combinedArray = self.__dfCheck('ItemName',combinedArray)
-        return(sum(array,[]))
-  
+        return(combinedArray)
+
 
     def __translatePattern(self,item):
+    # Extracts pattern if it exists then bins
         for pattern in self.patterns:
             testPattern = re.findall(pattern,item)
             if testPattern:
@@ -329,17 +331,18 @@ class itemReduce(Props):
                 return ['unsorted']
             else:
                 next
-    
+
     def __extractedPatterns(self):
         array = []
-        for item in self.df['ItemName']: 
+        for item in self.df['ItemName']:
             array.append(self.__translatePattern(item))
         combinedArray = sum(array,[])
         combinedArray= self.__dfCheck('ItemName',combinedArray)
         return(combinedArray)
-    
-    
+
+
     def __translateTexture(self,item):
+    # extracts texture if it is in name, then bins
         for texture in self.textures:
             testTexture = re.findall(texture,item)
             if testTexture:
@@ -348,7 +351,7 @@ class itemReduce(Props):
                 return ['unsorted']
             else:
                 next
-          
+
     def __extractedTextures(self):
         array = []
         for item in self.df['ItemName']:
@@ -356,8 +359,9 @@ class itemReduce(Props):
         combinedArray = sum(array,[])
         combinedArray = self.__dfCheck('ItemName',combinedArray)
         return(combinedArray)
-    
+
     def __translateFit(self,item):
+    # extracts fit if it exists, then bins
         for fit in self.fits:
             testFit = re.findall(fit,item)
             if testFit:
@@ -366,7 +370,7 @@ class itemReduce(Props):
                 return ['unsorted']
             else:
                 next
-          
+
     def __extractedFits(self):
         array = []
         for item in self.df['ItemName']:
@@ -378,7 +382,7 @@ class itemReduce(Props):
     def __dfCheck(self,dfItem,isoParam):
         dsI = len(self.df[dfItem])
         dsP = len(isoParam)
-        
+
         if dsI > dsP:
             dsDiff = dsI - dsP
             for i in range(dsDiff)[::-1]:
@@ -388,10 +392,10 @@ class itemReduce(Props):
             for i in range(dsDiff)[::-1]:
                 del isoParam[-i]
         return(isoParam)
-    
+
 class colorReduce:
     #returns pandas dataframe with reduced colors
-    
+
     def __init__(self,pdDF):
         self.pdDF = pdDF
         self.nordstromColors = self.pdDF['Color']
@@ -399,8 +403,8 @@ class colorReduce:
         self.basicColors = self.colorTdDict.keys()
         self.reducedColors = self.__colorListTranslate()
         self.__newDF()
-    
-    def __colorTDict(self): 
+
+    def __colorTDict(self):
         return {
             #Reds
             'Red':'Red',
@@ -437,12 +441,12 @@ class colorReduce:
             'Rosa':'Red',
             'Fuschia':'Red',
             'Rouge':'Red',
-    
+
             #Oranges
             'Orange':'Orange',
-            'Tangerine':'Orange', 
-            'Nectarine':'Orange', 
-            'Sunrise':'Orange', 
+            'Tangerine':'Orange',
+            'Nectarine':'Orange',
+            'Sunrise':'Orange',
             'Horizon':'Orange',
             'Cinnamon':'Orange',
             'Rust':'Orange',
@@ -464,7 +468,7 @@ class colorReduce:
             'Ambrosia':'Orange',
             'Canteloupe':'Orange',
             'Persimmon':'Orange',
-            
+
             #Yellows
             'Yellow':'Yellow',
             'Gold':'Yellow',
@@ -478,7 +482,7 @@ class colorReduce:
             'Wheat':'Yellow',
             'Lemondrop':'Yellow',
             'Sundrop':'Yellow',
-            
+
             #Greens
             'Green':'Green',
             'Moss':'Green',
@@ -512,13 +516,13 @@ class colorReduce:
             'Vernet':'Green',
             'Malachite':'Green',
             'Pear':'Green',
-    
+
             #Blues
             'Blue':'Blue',
             'Marine':'Blue',
             'Teal':'Blue',
             'Turquoise':'Blue',
-            'Navy':'Blue', 
+            'Navy':'Blue',
             'Denim':'Blue',
             'Atlantic':'Blue',
             'Aqua':'Blue',
@@ -531,7 +535,7 @@ class colorReduce:
             'Lake':'Blue',
             'Maritime':'Blue',
             'Wave':'Blue',
-            'Blueberry':'Blue', 
+            'Blueberry':'Blue',
             'Neptune':'Blue',
             'Cobalt':'Blue',
             'Cyan':'Blue',
@@ -548,12 +552,12 @@ class colorReduce:
             'Chartruse':'Blue',
             'Cornflower':'Blue',
             'Marina':'Blue',
-    
+
             #Purples
             'Purple':'Purple',
             'Lilac':'Purple',
             'Violet':'Purple',
-            'Lavender':'Purple', 
+            'Lavender':'Purple',
             'Periwinkle':'Purple',
             'Grape':'Purple',
             'Amethyst':'Purple',
@@ -562,7 +566,7 @@ class colorReduce:
             'Pomegranate':'Purple',
             'Raisin':'Purple',
             'Viola':'Purple',
-            
+
             #Blacks
             'Black':'Black',
             'Charcoal':'Black',
@@ -571,15 +575,15 @@ class colorReduce:
             'Caviar':'Black',
             'Coal':'Black',
             'Noir':'Black',
-    
+
             #Grays
-            'Gray':'Grey', 
+            'Gray':'Grey',
             'Silver':'Grey',
-            'Graphite':'Grey', 
+            'Graphite':'Grey',
             'Grey':'Grey',
             'Asphalt':'Grey',
             'Ash':'Grey',
-            'Stone':'Grey', 
+            'Stone':'Grey',
             'Pavement':'Grey',
             'Platinum':'Grey',
             'Granite':'Grey',
@@ -596,7 +600,7 @@ class colorReduce:
             'Cement':'Grey',
             'Flint':'Grey',
             'Gunmetal':'Grey',
-            
+
             #Whites
             'White':'White',
             'Cream':'White',
@@ -615,18 +619,18 @@ class colorReduce:
             'Whey':'White',
             'Alabaster':'White',
             'Pumice':'White',
-            
+
             #Browns
             'Brown':'Brown',
             'Coffee':'Brown',
-            'Khaki':'Brown', 
+            'Khaki':'Brown',
             'Tan':'Brown',
             'Beige':'Brown',
             'Camel':'Brown',
             'Natural':'Brown',
             'Oatmeal':'Brown',
-            'Chocolate':'Brown', 
-            'Cappuccino':'Brown', 
+            'Chocolate':'Brown',
+            'Cappuccino':'Brown',
             'Caramel':'Brown',
             'Mushroom':'Brown',
             'Desert':'Brown',
@@ -652,7 +656,7 @@ class colorReduce:
             'Tiramisu':'Brown',
             'Mocha':'Brown',
             }
-    
+
     def __colorTranslate(self,nColor):
         for color in self.basicColors:
             testColor = re.findall(color,nColor)
@@ -666,6 +670,34 @@ class colorReduce:
         for color in self.nordstromColors:
             reducedColors.append(self.__colorTranslate(color))
         return(reducedColors)
-        
+
     def __newDF(self):
         self.pdDF['binnedColor'] = self.reducedColors
+
+class ordinate(Props):
+    def __init__(self,df,sex):
+        self.terms = Props.__init__(self)
+        self.df = df
+        self.__dfOrd()
+        self.df['sex'] = sex
+
+    def __ordinate(self,val):
+        # returns index of the value in the dataset
+        if val == 'unsorted':
+            return 0
+        colors = ['Red','Orange','Yellow','Green','Blue','Purple','Black','Grey','White','Brown']
+        arrays = [self.items,self.fits,self.textures,self.patterns,colors]
+        for l in arrays:
+            try:
+                return(1 + l.index(val))
+            except:
+                next
+
+    def __dfOrd(self):
+        # make sa dataframe ordinal
+        for i in self.df.columns.tolist():
+            features = ['item','pattern','texture','fit','binnedColor'] # don't forget 'Price'
+            if i not in features:
+                next
+            else:
+                self.df[i] = map(lambda x: self.__ordinate(x),self.df[i].tolist())
